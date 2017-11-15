b89d404bd96f1b1c56b099ab361867f1b33ad0a7
thiadeliria <spec.thia@gmail.com>
2017-11-14 23:57:08 -0500

    adjusted map.php modal transparency

--- map.php
+++ map.php
@@ -37,7 +37,8 @@
 
 <p>TO-DO: <br>
 catch exceptions for countries w/o data; <br>
-write 'for a full list of countries click /here/' msg;
+write 'for a full list of countries click /here/' msg; <br>
+sync country names w/google;
 </p>
     
 </body>
