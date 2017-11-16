393986b1d9351ae31bb1f6f935bf4e0abe625f45
Zhaeong <owen_zzz@hotmail.com>
2017-11-15 23:21:34 -0500

    reposition the google api script after map pages

--- about.php
+++ about.php
@@ -2,9 +2,6 @@
 <html>
 <head>
   <link rel="stylesheet" href="css/indexstyle.css">
-  <script src="js/indexpage.js"></script> 
-  <script src="js/mappage.js"></script> 
-  
 
   <title>About</title>
 </head>
