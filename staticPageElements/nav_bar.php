a2388fae8dc0e53001e14bddd3e0479ba5c29fed
thiadeliria <spec.thia@gmail.com>
2018-11-14 20:54:35 -0500

    Updated navbar html: separated title (left) from links (right) and added link for dropdown icon

--- staticPageElements/nav_bar.php
+++ staticPageElements/nav_bar.php
@@ -1,7 +1,7 @@
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!--NAVIGATION BAR-->
-<div class="navbar">
+<!-- <div class="navbar">
   <a href="index.php" id="title">
         <strong> worldplug </strong>
   </a>
@@ -9,8 +9,24 @@
   <a href="list.php" class="navbarlinks">LIST</a>
   <a href="map.php" class="navbarlinks">MAP</a>
   <a href="index.php" class="navbarlinks">HOME</a>
-</div>
+</div> -->
 
+<!--RESPONSIVE NAVIGATION BAR-->
+<div class="topnav" id="navBar">
+  <a href="index.php" id="title">
+        <strong>worldplug</strong>
+  </a>
+  <div class="topnav-right">
+      <a href="about.php" >ABOUT</a>
+      <a href="list.php">LIST</a>
+      <a href="map.php">MAP</a>
+      <a href="index.php">HOME</a>
+      <!-- this link opens and closes the topnav on small screens -->
+      <a href="javascript:void(0);" class="icon" onclick="toggleMenuDropdown()">
+        <i class="fa fa-bars"></i>
+      </a>
+  </div>
+</div>
 
 <!--FOOTER-->
 <div id="footer">
