11fd3ed5302891dd358cc78e4908d8e2ed8dbfae
thiadeliria <spec.thia@gmail.com>
2018-11-21 16:39:19 -0500

    Modified navbar skeleton

--- staticPageElements/nav_bar.php
+++ staticPageElements/nav_bar.php
@@ -1,31 +1,28 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<!--NAVIGATION BAR-->
-<!-- <div class="navbar">
-  <a href="index.php" id="title">
-        <strong> worldplug </strong>
-  </a>
-  <a href="about.php" class="navbarlinks">ABOUT</a>
-  <a href="list.php" class="navbarlinks">LIST</a>
-  <a href="map.php" class="navbarlinks">MAP</a>
-  <a href="index.php" class="navbarlinks">HOME</a>
-</div> -->
-
 <!--RESPONSIVE NAVIGATION BAR-->
 <div class="topnav" id="navBar">
-  <a href="index.php" id="title">
-        <strong>worldplug</strong>
-  </a>
-  <div class="topnav-right">
-      <a href="about.php" >ABOUT</a>
-      <a href="list.php">LIST</a>
-      <a href="map.php">MAP</a>
-      <a href="index.php">HOME</a>
-      <!-- this link opens and closes the topnav on small screens -->
-      <a href="javascript:void(0);" class="icon" onclick="toggleMenuDropdown()">
-        <i class="fa fa-bars"></i>
-      </a>
-  </div>
+    <a href="index.php" style="float: left;"> 
+        <img src="../images/title.png" height="100px" width="600px">
+    </a>
+    <div class="topnav-links">
+        <a href="about.php" >ABOUT</a>
+        <a href="list.php">LIST</a>
+        <a href="map.php">MAP</a>
+        <a href="index.php">HOME</a>
+        <div class="dropdown">
+            <!-- this link opens and closes the topnav on small screens -->
+            <a href="javascript:void(0);" class="burgerIcon" onclick="toggleMenuDropdown()">
+                 <i class="fa fa-bars"></i>
+            </a>
+            <div class="dropdown-content" id="dropdownLinks">
+                <a href="index.php">HOME</a>
+                <a href="map.php">MAP</a>
+                <a href="list.php">LIST</a>
+                <a href="about.php" >ABOUT</a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!--FOOTER-->
